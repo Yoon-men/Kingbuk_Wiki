6da2fbdac770b1b28a65936e29c2ac521749d97a
Yoontaeyeong <64591335+Yoontaeyeong@users.noreply.github.com>
2020-07-16 21:45:33 +0900

    킹북위키 v1.0

---
+++ secret_code2.js
@@ -0,0 +1,19 @@
+function 비밀코드2(){
+    if(prompt('2번 비밀 코드를 입력하세요.') == '킹준달'){
+        alert('킹준달위키로 이동합니다.');
+    }
+
+    else {
+        alert('오류');
+    }
+}
+/*
+function func_confirm() {
+
+    if (confirm('준달위키 만들면 좋을 것 같아요?')) {
+        alert('ㅇㅋ');
+    } else {
+        alert('응 알았어');
+    }
+}
+ */
