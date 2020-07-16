9746bd3720fbeec8e0a246f13b8f08f478dfdb7c
Yoontaeyeong <64591335+Yoontaeyeong@users.noreply.github.com>
2020-07-17 00:14:50 +0900

    Update secret_code1.js

--- secret_code1.js
+++ secret_code1.js
@@ -1,5 +1,5 @@
 function 비밀코드1(){
-    if(prompt('1번 비밀 코드를 입력하세요.') == '킹민'){
+    if(prompt('1번 비밀 코드를 입력하세요.') == 'kimmax'){
         alert('킹민위키로 이동합니다.');
         open('킹민위키.html')
     }
@@ -8,16 +8,3 @@ function 비밀코드1(){
         alert('취소되었습니다.');
     }
 }
-
-/*
-function func_prompt() {
-    var secret_code = prompt("비밀 코드를 입력하세요."); // var : 변수, prompt : 사용자 입력값 받기
-    if (secret_code) =='kingmin' {
-        alert("킹민위키로 이동합니다");
-    }
-    else {
-        alert("잘못된 코드입니다");
-    }
-}
-
- */
