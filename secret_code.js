20c44b48b5286ec2de5846f016a75768a040b683
Yoontaeyeong <64591335+Yoontaeyeong@users.noreply.github.com>
2020-07-17 19:43:25 +0900

    Update secret_code.js

--- secret_code.js
+++ secret_code.js
@@ -1,7 +1,7 @@
 function 비밀코드1(){
     if(prompt('1번 비밀 코드를 입력하세요.') == 'kimmax'){
         alert('킹민위키로 이동합니다.');
-        open('킹민위키.html')
+        open('킹민위키.html');
     }
 
     else {
@@ -10,8 +10,10 @@ function 비밀코드1(){
 }
 
 function 비밀코드2(){
-    if(prompt('2번 비밀 코드를 입력하세요.') == '킹준달'){
-        alert('킹준달위키는 아직 준비 중입니다.');
+    if(prompt('2번 비밀 코드를 입력하세요.') == 'DJ권'){
+        alert('킹준달위키로 이동합니다.');
+        open('킹준달위키.html');
+
     }
 
     else {
