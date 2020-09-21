1d4211ff915258f46a7f2048bb51d41f9641bbe9
Yoon-men <64591335+Yoon-men@users.noreply.github.com>
2020-09-21 12:30:18 +0900

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-function clac() {
-    var currentYear = 2020;
-    var birthyear = prompt("태어난 연도를 입력하셈.", "YYYY");
-    var age;
-    age = currentYear - birthyear + 1;
-    document.querySelector("#result").innerHTML="너의 나이는" + age + "살이다.";
-}
