69dcff653f625eaf3e2466492f94dd70c4795240
Yoonmen <64591335+Yoon-men@users.noreply.github.com>
2020-11-06 02:44:28 +0900

    특정 코드 입력 시 이동

---
+++ index.js
@@ -0,0 +1,35 @@
+function pageOpen() {
+    var objName = document.getElementById("secretcord");
+    if (objName.value == "") {
+        alert("발급된 비밀코드를 입력해주세요");
+        objName.focus();
+        return;
+    }
+
+    if (objName.value == "김MAX"){
+        alert("킹민위키로 이동합니다.")
+        open('킹민위키.html')
+    }
+
+    if (objName.value == "DJ권"){
+        alert("킹준달위키로 이동합니다.")
+        open('킹준달위키.html')
+    }
+
+    if (objName.value == "갓시흔"){
+        alert("갓시흔위키는 아직 준비 중입니다.")
+    }
+
+    if (objName.value == "갓상준"){
+        alert("갓상준위키는 아직 준비 중입니다.")
+    }
+
+    else {
+        alert("올바른 코드를 입력해주세요.")
+    }
+}
+
+// font-family: 'Black Han Sans', sans-serif;
+// font-family: 'Nanum Gothic', sans-serif;
+// font-family: 'Neucha', cursive;
+// font-family: 'Song Myung', serif;
